6b425e48cdd64dcf25f233365ee527c558e22fe9
Don Browning <donwb@dons-imac-pro.turner.com>
2018-11-21 12:44:06 -0500

    added os check

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from sys import platform
 
 import urllib, os, urllib.request
 import time
 
-chrome_options = Options()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument("window-size=1024,768")
-chrome_options.add_argument("--no-sandbox")
+print(platform)
 
-driver = webdriver.Chrome(chrome_options=chrome_options)
+if platform == "linux" or platform =="linux2":
+    print('Using Linux driver...')
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("window-size=1024,768")
+    chrome_options.add_argument("--no-sandbox")
+
+    #driver = webdriver.Chrome(chrome_options=chrome_options)
+else:
+    print('Using MacOS driver...')
+    driver = webdriver.Safari()
 
 driver.get("http://www.python.org")
 assert "Python" in driver.title
